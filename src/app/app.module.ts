802a767240e447b042ae7f106fb1215371cef144
TrCaM <cmthscb@gmail.com>
2018-03-18 21:07:59 -0400

    Finishing basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { FileComponent } from './file/file.component';
+import { MainBoardComponent } from './main-board/main-board.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SplitPaneModule } from 'ng2-split-pane/lib/ng2-split-pane';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileComponent,
+    MainBoardComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    SplitPaneModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
